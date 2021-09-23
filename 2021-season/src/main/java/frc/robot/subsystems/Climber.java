15304e65d223b37f419cf51405366a4f15aba6dc
Tumi Oguntola <tumi.oguntola@gmail.com>
2021-09-23 14:22:24 -0400

    Additional framework for climber subsystem

--- 2021-season/src/main/java/frc/robot/subsystems/Climber.java
+++ 2021-season/src/main/java/frc/robot/subsystems/Climber.java
@@ -10,10 +10,35 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Climber extends SubsystemBase {
-    public WPI_TalonSRX climber1 = new WPI_TalonFX;
-    public WPI_TalonSRX climber2 = new WPI_TalonFX;
+    public CANSparkMax leftClimberMotor = new CANSparkMax(5, MotorType.kBrushless);
+    public CANSparkMax rightClimberMotor = new CANSparkMax(6, MotorType.kBrushless);
 
-    public Climber(){
-        
+    public static Solenoid intakeSolenoid1 = new Solenoid(50, 4);
+
+    public void runMotorsForward(double power) {
+        leftClimberMotor.set(power);
+        rightClimberMotor.set(power);
     }
+    public void runMotorsBackward(double power) {
+        leftClimberMotor.set(-1 * power);
+        rightClimberMotor.set(-1* power);
+    }
+    public void stopMotors() {
+        leftClimberMotor.set(0.0);
+        rightClimberMotor.set(0.0);
+    }
+
+    public void spinClockwise() {
+        boolean clockwiseSpin = Robot.controllerSecondary.getBumper(Hand.kRight);
+        if(clockwiseSpin) {
+            runMotorsForward(0.1); // Reverse intake
+        }
+      }
+
+    public void spinCounterClockwise() {
+        boolean counterclockSpin = Robot.controllerSecondary.getBumper(Hand.kLeft);
+        if(counterclockSpin) {
+            runMotorsBackward(0.1); // Reverse intake
+        }
+      }
 }
